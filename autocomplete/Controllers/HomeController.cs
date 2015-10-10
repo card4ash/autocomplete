a6f1cc9ec5daad258f06ac4fadea0468f394e9d2
ashraful <ash5rifat@gmail.com>
2015-10-10 14:37:51 +0600

    first commit

---
+++ autocomplete/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using autocomplete.Models;
+
+namespace autocomplete.Controllers
+{
+    public class HomeController : Controller
+    {
+        autocomplete.DataAccess.PollidutEntities db = new DataAccess.PollidutEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult FetchTagList(string tag)
+        {
+            var tags = db.EMPLOYEES.Where(e => e.EMPLOYEE_TYPE_ID == 2 && e.PERSON.PERSON_NAME.Contains(tag)).Select(e => new Tag
+            {
+                ID = e.EMPLOYEE_ID,
+                TagName = e.PERSON.PERSON_NAME
+            }).ToList();
+            return Json(new { status="success",data=tags});
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
